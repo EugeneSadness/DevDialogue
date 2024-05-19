c80e9c05e205c1abe1a623729d4e008d25ccbace
EugeneSadness <jinux5681@gmail.com>
2024-05-19 12:09:33 +0600

    edit gitignore file

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require("sequelize");
 require('dotenv').config();
-
 module.exports = new Sequelize(process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,{
