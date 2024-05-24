52de0bf3c0fa16a232ff333a310a0760b972c29f
EugeneSadness <jinux5681@gmail.com>
2024-05-24 21:39:34 +0600

    we can chat!

--- frontend/src/Chat/Chat.jsx
+++ frontend/src/Chat/Chat.jsx
@@ -49,7 +49,7 @@ function Chat() {
 
 
 
-    const sendMessageAndPicture = () => {
+    const sendMessageAndPicture = async () => {
         const messageData = { content: message, senderId: userid, username: username, chatId: chatId };
         socket.emit('chatMessage', messageData);
         setMessages(prevMessages => [...prevMessages,
@@ -77,8 +77,8 @@ function Chat() {
 
     const deleteAllMessagesFromChat = async () => {
         try {
-            const response = await Axios.post(process.env.REACT_APP_BACK_URL + '/api/message/delAllMessagesFromChat', 
-            { chatId });
+            const response = await Axios.post(process.env.REACT_APP_BACK_URL + '/api/message/delAllMessagesFromChat',
+                { chatId });
             setMessages([]);
             setMessage('');
         } catch (error) {
@@ -88,8 +88,8 @@ function Chat() {
 
     const findFriend = async (friendName) => {
         try {
-            const response = await Axios.post(process.env.REACT_APP_BACK_URL + '/api/user/findUserByName', 
-            { name: friendName });
+            const response = await Axios.post(process.env.REACT_APP_BACK_URL + '/api/user/findUserByName',
+                { name: friendName });
             const userData = response.data;
             setModalAddUserWindowData(userData);
         } catch (error) {
@@ -99,121 +99,121 @@ function Chat() {
 
     const addFriendToChat = async (friendData) => {
         try {
-            const response = await Axios.post(process.env.REACT_APP_BACK_URL + '/api/chat/addUserToChat', 
-            { chatId: chatId, recieverId: friendData.userid, inviterId: userid });
+            const response = await Axios.post(process.env.REACT_APP_BACK_URL + '/api/chat/addUserToChat',
+                { chatId: chatId, recieverId: friendData.userid, inviterId: userid });
         } catch (error) {
             console.error(error.response.data.message);
         }
     };
 
-        useEffect(() => {
-            fetchMessagesFromDatabase();
-        }, []);
+    useEffect(() => {
+        fetchMessagesFromDatabase();
+    }, []);
 
-        useEffect(() => {
-            socket.on("chat message", async (data) => {
-                const isMessageAlreadyPresent = messages.some(
-                    (msg) => msg.content === data.content && msg.senderId === data.senderId && msg.username === data.username
-                );
+    useEffect(() => {
+        socket.on("chat message", async (data) => {
+            const isMessageAlreadyPresent = messages.some(
+                (msg) => msg.content === data.content && msg.senderId === data.senderId && msg.username === data.username
+            );
 
-                if (!isMessageAlreadyPresent) {
-                    setMessages((prevMessages) => [...prevMessages, data]);
-                }
-            });
-            return () => socket.off('chat message');
-        }, [messages, socket]);
+            if (!isMessageAlreadyPresent) {
+                setMessages((prevMessages) => [...prevMessages, data]);
+            }
+        });
+        return () => socket.off('chat message');
+    }, [messages, socket]);
 
-        const openModalUsernameWindow = () => {
-            setModalUsernameWindowIsOpen(true)
-        }
+    const openModalUsernameWindow = () => {
+        setModalUsernameWindowIsOpen(true)
+    }
 
-        const closeModalUsernameWindow = () => {
-            setModalUsernameWindowIsOpen(false)
-        }
-        const closeModalAddUserWindow = () => {
-            setModalAddUserWindowIsOpen(false)
-        }
+    const closeModalUsernameWindow = () => {
+        setModalUsernameWindowIsOpen(false)
+    }
+    const closeModalAddUserWindow = () => {
+        setModalAddUserWindowIsOpen(false)
+    }
 
-        const openModalAddUserWindow = () => {
-            setModalAddUserWindowIsOpen(true)
-        }
+    const openModalAddUserWindow = () => {
+        setModalAddUserWindowIsOpen(true)
+    }
 
-        return (
-            <div className="UserForm" id={theme}>
-                <button className="log-out-button" onClick={handleBackToChats}>Back to chats</button>
-                <span className="heading">
-                    <h2 className='chat-title'>Chat: {chatName}</h2>
-                    <button className='add-user-button' onClick={openModalAddUserWindow}>
+    return (
+        <div className="UserForm" id={theme}>
+            <button className="log-out-button" onClick={handleBackToChats}>Back to chats</button>
+            <span className="heading">
+                <h2 className='chat-title'>Chat: {chatName}</h2>
+                <button className='add-user-button' onClick={openModalAddUserWindow}>
                     Add user to chat!
-                    </button>
-                </span>
-                <div className="chat-container">
-                    <div style={{ color: theme === "light" ? "black" : "yellow" }} className="messages">
-                        <ul>
-                            {messages.map((msg, index) => (
-                                <li
-                                    key={index}
-                                    className={`${msg.senderId === userid ? "sent" : "received"}`}
-                                >
-                                    <button className='username-button' onClick={openModalUsernameWindow}>
-                                        {msg.username}
-                                    </button>:
-                                    {msg.content}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
+                </button>
+            </span>
+            <div className="chat-container">
+                <div style={{ color: theme === "light" ? "black" : "yellow" }} className="messages">
+                    <ul>
+                        {messages.map((msg, index) => (
+                            <li
+                                key={index}
+                                className={`${msg.senderId === userid ? "sent" : "received"}`}
+                            >
+                                <button className='username-button' onClick={openModalUsernameWindow}>
+                                    {msg.username}
+                                </button>:
+                                {msg.content}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
+            </div>
 
-                <div className="input">
+            <div className="input">
+                <input
+                    className="messages"
+                    placeholder="Message..."
+                    value={message}
+                    onChange={(event) => setMessage(event.target.value)}
+                />
+                <button onClick={sendMessageAndPicture}>Send message</button>
+
+
+            </div>
+
+            <Modal isOpen={modalUsernameModalWindowIsOpen} onRequestClose={closeModalUsernameWindow} ariaHideApp={false} className='modal-window-user-info'>
+                <h2>User info</h2>
+                <form className='modal-username-form'>
+                    <option>Email: {email}</option>
+                    <option>Name:  {username}</option>
+
+                </form>
+            </Modal>
+
+            <Modal isOpen={modalAddUserModalWindowIsOpen} onRequestClose={closeModalAddUserWindow} ariaHideApp={false} className='modal-window-add-user'>
+                <span>
+                    <h2>Add users:</h2>
                     <input
-                        className="messages"
-                        placeholder="Message..."
-                        value={message}
-                        onChange={(event) => setMessage(event.target.value)}
+                        className="friendName"
+                        placeholder="Search friend by name"
+                        value={friendName}
+                        onChange={(event) => setFriendName(event.target.value)}
                     />
-                    <button onClick={sendMessageAndPicture}>Send message</button>
+                    <button onClick={() => findFriend(friendName)}>Search</button>
+                    {modalAddUserWindowData && (
+                        <div className="user-info">
+                            <p>Email: {modalAddUserWindowData.email}</p>
+                            <p>Name: {modalAddUserWindowData.username}</p>
+                            <button onClick={() => addFriendToChat(modalAddUserWindowData)}>Add {modalAddUserWindowData.username} to chat</button>
+                        </div>
+                    )}
+                </span>
+            </Modal>
 
-
-                </div>
-
-                <Modal isOpen={modalUsernameModalWindowIsOpen} onRequestClose={closeModalUsernameWindow} ariaHideApp={false} className='modal-window-user-info'>
-                    <h2>User info</h2>
-                    <form className='modal-username-form'>
-                        <option>Email: {email}</option>
-                        <option>Name:  {username}</option>
-
-                    </form>
-                </Modal>
-
-                <Modal isOpen={modalAddUserModalWindowIsOpen} onRequestClose={closeModalAddUserWindow} ariaHideApp={false} className='modal-window-add-user'>
-                    <span>
-                        <h2>Add users:</h2>
-                        <input
-                            className="friendName"
-                            placeholder="Search friend by name"
-                            value={friendName}
-                            onChange={(event) => setFriendName(event.target.value)}
-                        />
-                        <button onClick={() => findFriend(friendName)}>Search</button>
-                        {modalAddUserWindowData && (
-                            <div className="user-info">
-                                <p>Email: {modalAddUserWindowData.email}</p>
-                                <p>Name: {modalAddUserWindowData.username}</p>
-                                <button onClick={() => addFriendToChat(modalAddUserWindowData)}>Add {modalAddUserWindowData.username} to chat</button>
-                            </div>
-                        )}
-                    </span>
-                </Modal>
-
-                {/* Delete All Messages Button
+            {/* Delete All Messages Button
             <div className="delete-all-button">
                 <button className="delete-button" onClick={deleteAllMessagesFromChat}>Delete All Messages</button>
             </div>
             */}
-            </div>
-        );
+        </div>
+    );
 
-    }
+}
 
-    export default Chat;
+export default Chat;
